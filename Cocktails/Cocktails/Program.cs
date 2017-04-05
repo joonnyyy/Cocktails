0422e61fdb8cc83633cc48c5549371eb95bf0321
Manuel <Manuel@LAPTOP-6J5NBBLQ>
2017-04-05 10:41:05 +0200

    rdsa

--- Cocktails/Cocktails/Program.cs
+++ Cocktails/Cocktails/Program.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
-namespace Cocktails
+namespace SQL_Databse_Demo_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("tfaoiufuisiufwiuo4gfest123s");
+            {
+                // http://www.connectionstrings.com/sql-server/
+                // use "." for local server like: '.\myInstanceName'
+                string connectionString = @"Server=192.168.56.101;Database=Cocktail_Project;User Id=Bumsti;Password=3uuabe4A;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string commandstring = "SELECT * FROM Cocktails";
+                    SqlCommand command = new SqlCommand(commandstring, connection);
+                    SqlDataReader reader;
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["CID"] + "\t" + reader["Preis"] + "\t" + reader["Beschreibung"] + "\t" + reader["Bild"] + "\t" + reader["Bewertung"] + "\t" + reader["KatID"]);
+
+                    }
+                    Console.ReadKey();
+                }
+
+
+            }
         }
     }
 }
